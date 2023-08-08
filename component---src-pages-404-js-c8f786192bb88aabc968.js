b255cd919d71826ea7003bd5c398f7380283068d
tujoworker <tujoworker@users.noreply.github.com>
2023-08-08 13:30:53 +0000

    deploy: aa16519fe13e484d75913839dc57493fd9a4428a

--- component---src-pages-404-js-c8f786192bb88aabc968.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkexample_basic=self.webpackChunkexample_basic||[]).push([[883],{2039:function(e,n,c){c.r(n),c.d(n,{default:function(){return a}});var u=c(6521);function a(){return(0,u.jsx)("main",{children:(0,u.jsx)("h1",{children:"404"})})}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c8f786192bb88aabc968.js.map
